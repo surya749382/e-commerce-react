78431c25b328a821bd98f53bd621cc8bf1e67c5b
surya <suryap.7493@gmail.com>
2025-01-26 12:59:25 +0530

    order completed

--- src/customer/components/Order/OrderDetails.jsx
+++ src/customer/components/Order/OrderDetails.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import AddressCard from "../AddressCard/AddressCard";
 import OrderTracker from "./OrderTracker";
 import { Box, Grid } from "@mui/material";
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import { deepPurple } from '@mui/material/colors';
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import { deepPurple } from "@mui/material/colors";
 
 const OrderDetails = () => {
   return (
@@ -15,35 +15,43 @@ const OrderDetails = () => {
       <div className="py-20">
         <OrderTracker activeStep={3}> </OrderTracker>
       </div>
-      <Grid className="space-x-5" container>
-        <Grid
-          item
-          container
-          className="shadow-xl rounded-md border"
-          sx={{ alignItems: "center", justifyContent: "space-between" }}
-        >
-          <Grid item xs={6}>
-            <img
-              className="w-[5rem] h-[5rem] object-cover object-top"
-              src="https://rukminim1.flixcart.com/image/612/612/l5h2xe80/kurta/x/6/n/xl-kast-tile-green-majestic-man-original-imagg4z33hu4kzpv.jpeg?q=70"
-              alt=""
-            />
-            <div className="ml-5 space-y-2 ">
-              <p className=" ">Men slim mid Rise Black jeans</p>
-              <p className="space-x-5">
-                <span>Color : Pink </span>
-                <span>Size : M </span>
-              </p>
-              <p>Seller: Siyarams</p>
-              <p>Price : 1055 </p>
-            </div>
+      <Grid className="space-y-5 " container>
+      {[1, 1, 1, 1, 1].map((item) => (
+          <Grid
+            item
+            container
+            className="shadow-xl rounded-md border mx-2 p-5"
+            sx={{ alignItems: "center", justifyContent: "space-between" }}
+          >
+            <Grid item xs={6}>
+              <div className="flex items-center my-4 space-x-4  ">
+                <img
+                  className="w-[5rem] h-[5rem] object-cover object-top"
+                  src="https://rukminim1.flixcart.com/image/612/612/l5h2xe80/kurta/x/6/n/xl-kast-tile-green-majestic-man-original-imagg4z33hu4kzpv.jpeg?q=70"
+                  alt=""
+                />
+                <div className="ml-5 space-y-2  ">
+                  <p className=" font-semibold">
+                    Men slim mid Rise Black jeans
+                  </p>
+                  <p className="space-x-5 opacity-50 text-xs font-semibold">
+                    <span>Color : Pink </span>
+                    <span>Size : M </span>
+                  </p>
+                  <p>Seller: Siyarams</p>
+                  <p>Price : 1055 </p>
+                </div>
+              </div>
+            </Grid>
+
+            <Grid item>
+              <Box sx={{ color: deepPurple[500] }}>
+                <StarBorderIcon sx={{ fontSize: "1rem" }} />
+                <span>Rate & Review Product</span>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Box sx={{ color:deepPurple[500] }}>
-              <StarBorderIcon sx={{fontSize:"2rem"}}/>
-            </Box>
-          </Grid>
-        </Grid>
+      ))}
       </Grid>
     </div>
   );
